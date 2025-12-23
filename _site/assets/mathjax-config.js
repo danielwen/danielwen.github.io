ccd071fc5f0fb402908853512ed58cc63a9e01bb
Daniel Wen <danielwen@users.noreply.github.com>
2025-12-22 22:22:38 -0500

    Initialize hugo

--- _site/assets/mathjax-config.js
+++
@@ -1,9 +0,0 @@
-MathJax.Hub.Config({
-    messageStyle: "none",
-    MMLorHTML: {
-        prefer: {
-            Firefox: "MML"
-        }
-    }
-});
-MathJax.Ajax.loadComplete("//lane-widen.com/assets/mathjax-config.js");
--- error.md
+++
@@ -1,7 +0,0 @@
----
-layout: page
-permalink: /error/
-title: Error occurred
----
-
-An error has occured. Please return to the [home page](/) or try again later.
